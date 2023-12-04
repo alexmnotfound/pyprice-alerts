47faf96db6e49033d186ec88fae6d637a843baa0
alexmnotfound <matias.alrd@gmail.com>
2023-12-04 12:04:51 -0300

    Modified to receive variables

--- config/settings.py
+++ config/settings.py
@@ -2,3 +2,7 @@ import os
 
 # Fetch the Telegram bot token from an environment variable
 TELEGRAM_API_TOKEN = os.getenv('TELEGRAM_API_TOKEN')
+
+# Google Sheets configuration
+SHEET_ID = '1YtiY-KXPj3kBhye5QPkAqYnq5uQZpnTk34o_U2Bn10M'
+SHEET_RANGE = 'Watchlist!B8:Z'
