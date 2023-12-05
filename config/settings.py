3dff613e0d65d883f42f11ec0abac3a1e435d45c
alexmnotfound <matias.alrd@gmail.com>
2023-12-05 15:56:59 -0300

    Added some useful things

--- config/settings.py
+++ config/settings.py
@@ -3,6 +3,10 @@ import os
 # Fetch the Telegram bot token from an environment variable
 TELEGRAM_API_TOKEN = os.getenv('TELEGRAM_API_TOKEN')
 
+# Some useful chat's ID
+MR_CHAT_ID = '808918721'
+OJ_CHAT_ID = '1571968446'
+
 # Google Sheets configuration
 SHEET_ID = '1YtiY-KXPj3kBhye5QPkAqYnq5uQZpnTk34o_U2Bn10M'
 SHEET_RANGE = 'Watchlist!B8:Z'
